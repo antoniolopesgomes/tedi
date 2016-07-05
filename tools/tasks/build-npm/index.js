dec532c11e387d4855825d9a04bcf9ac5cd49e89
António Gomes <antoniolopesgomes@gmail.com>
2016-07-05 16:07:56 +0100

    Refactored gulp tasks

--- tools/tasks/build-npm/index.js
+++ tools/tasks/build-npm/index.js
@@ -1,62 +1,34 @@
 var gulp = require('gulp');
-var gTs = require('gulp-typescript');
 var gSeq = require('gulp-sequence');
 var merge = require('merge2');
 var del = require('del');
 var fs = require('fs');
 var gSym = require('gulp-sym');
 
-const TSCONFIG = '../../../tsconfig.json'
-const PACKAGE_JSON = '../../../package.json'
-const BUILD_PATH = 'dist/npm';
+//REUIRE TASKS DEPENDENCIES
+require('../typescript');
+
+//CONFIGURATION
+var config = require('../config');
 
 //CLEAN
-
 gulp.task('npm:clean', function () {
     return del([
-        BUILD_PATH + '/**',
+        config.BUILD_PATH + '/**',
     ]);
 })
 
-//TYPESCRIPT COMPILATION
-
-var tsconfig = require(TSCONFIG).compilerOptions;
-//emit declaration files (.d.ts)
-tsconfig.declaration = true;
-tsconfig.module = 'commonjs';
-//TODO Don't forget to change to typescript 2.0 when it is released
-tsconfig.typescript = require('typescript');
-
-var tsProject = gTs.createProject(tsconfig);
-
-gulp.task('npm:typescript', function () {
-    //compile
-    var tsResult = gulp
-        .src([
-            'src/**/*.ts',
-            '!src/examples/**',
-            'typings/**/*.ts',
-            'node_modules/inversify-dts/inversify/**/*.ts'
-        ])
-        .pipe(gTs(tsProject));
-
-    return merge([
-        tsResult.dts.pipe(gulp.dest(BUILD_PATH)),
-        tsResult.js.pipe(gulp.dest(BUILD_PATH))
-    ]);
-});
-
 //COPY FILES
-
 gulp.task('npm:copy-files', function () {
     return merge(
         gulp.src(['package.json', 'README.md', 'LICENSE'])
-            .pipe(gulp.dest(BUILD_PATH)),
+            .pipe(gulp.dest(config.BUILD_PATH)),
         gulp.src(['tools/tasks/build-npm/files/spec/**/*'])
-            .pipe(gulp.dest(BUILD_PATH + '/spec'))
+            .pipe(gulp.dest(config.BUILD_PATH + '/spec'))
     )
 });
 
+//CREATE SYMLINK IN NODE_MODULES
 gulp.task('npm:symlink', function (done) {
     return fs.symlink('d:/projects/tedi/dist/npm', 'node_modules/tedi', function (error) {
         console.log(error);
@@ -64,15 +36,15 @@ gulp.task('npm:symlink', function (done) {
     });
 });
 
-gulp.task('npm:build', gSeq(
+gulp.task('build', gSeq(
     'npm:clean',
-    'npm:typescript',
+    'typescript',
     'npm:copy-files')
 );
 
-gulp.task('build', ['npm:build'], function () {
+gulp.task('build:watch', ['build'], function () {
     gulp.watch(
         ['src/**/*.ts', '!src/examples/**'],
-        ['npm:typescript']
+        ['typescript']
     );
 });
---
+++ tools/tasks/typescript.js
@@ -0,0 +1,35 @@
+//TYPESCRIPT COMPILATION
+var gulp = require('gulp');
+var gTs = require('gulp-typescript');
+var merge = require('merge2');
+
+//CONFIG
+var config = require('./config');
+
+const TSCONFIG = '../../tsconfig.json'
+
+var tsconfig = require(TSCONFIG).compilerOptions;
+//emit declaration files (.d.ts)
+tsconfig.declaration = true;
+tsconfig.module = 'commonjs';
+//TODO Don't forget to change to typescript 2.0 when it is released
+tsconfig.typescript = require('typescript');
+
+var tsProject = gTs.createProject(tsconfig);
+
+gulp.task('typescript', function () {
+    //compile
+    var tsResult = gulp
+        .src([
+            'src/**/*.ts',
+            '!src/examples/**',
+            'typings/**/*.ts',
+            'node_modules/inversify-dts/inversify/**/*.ts'
+        ])
+        .pipe(gTs(tsProject));
+
+    return merge([
+        tsResult.dts.pipe(gulp.dest(config.BUILD_PATH)),
+        tsResult.js.pipe(gulp.dest(config.BUILD_PATH))
+    ]);
+});
